4ef4222bae072e6d4c504b28dff87cf9ea567085
Eecodev <mnasser.bbk@gmail.com>
2024-01-17 16:10:33 +0100

    technology store and update created

---
+++ app/Http/Requests/UpdateTechnologyRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+
+            'name' => ['required', 'min:3', 'max:200', Rule::unique('categories')->ignore($this->category)],
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+
+            'name.required' => 'Il name è obbligatorio',
+            'name.min' => 'Il name deve avere almeno :min caratteri',
+            'name.max' => 'Il name deve avere massimo :max caratteri',
+            'name.unique' => 'Questo name esiste già',
+        ];
+
+    }
+}
